1068e2527587f90b50c0fa88ad9902bfe428fd8d
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-05-21 21:42:38 +0200

    meething

--- scripts/meething.js
+++ scripts/meething.js
@@ -1,4 +1,6 @@
 function join() {
+  try {
+    console.log('trying to join...');
     const joinButton = document.getElementById('randomGo');
     if (!joinButton) {
         return setTimeout(join, 1000);
@@ -6,5 +8,7 @@ function join() {
     console.log(`Joining the room`);
     // join the room
     joinButton.click();
+  } catch(e) { console.error(e); }
 }
-join();
+console.log('injecting...');
+setTimeout(join, 2000);
